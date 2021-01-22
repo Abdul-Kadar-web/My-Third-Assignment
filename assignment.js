1b8ce7aa29c11bb283526ad54fd3bf0d240fc7f8
agforhad <abdulgaffer1100@gmail.com>
2021-01-22 17:47:46 +0600

    assignment

--- assignment.js
+++ assignment.js
@@ -2,68 +2,67 @@
 
 
 
-     //kilometerToMeter
+      kilometerToMeter
 
-// function kilometerToMeter(kilometer) {
-//     var meter = 1000 * kilometer;
-//     return meter;
-// }
-// var meter = kilometerToMeter(10);
-// console.log(meter);
+function kilometerToMeter(kilometer) {
+    var meter = 1000 * kilometer;
+    return meter;
+}
+var meter = kilometerToMeter(10);
+console.log(meter);
 
 
 
-//  budgetCalculator
+     budgetCalculator
 
-// function budgetCalculator(watch, mobile, laptop){
-//     var totalprice = 0;
-//     if(watch >= 0 && mobile >= 0 && laptop >= 0){
-//        var watchPrice = watch * 50;
-//        var mobilePrice = mobile * 100;
-//        var laptopPrice = laptop * 500;
-//        totalprice = watchPrice + mobilePrice + laptopPrice;
-//     }
-//     else{
-//         return('answer is negative')
-//     }
-//     return totalprice;
-// }
+function budgetCalculator(watch, mobile, laptop){
+    var totalprice = 0;
+    if(watch >= 0 && mobile >= 0 && laptop >= 0){
+       var watchPrice = watch * 50;
+       var mobilePrice = mobile * 100;
+       var laptopPrice = laptop * 500;
+       totalprice = watchPrice + mobilePrice + laptopPrice;
+    }
+    else{
+        return('answer is negative')
+    }
+    return totalprice;
+}
     
-// var numbers = budgetCalculator(20, 10, 5);
-// console.log(numbers);
+var numbers = budgetCalculator(20, 10, 5);
+console.log(numbers);
 
 
 
 
-// hotelCost
+     hotelCost
 
-// function hotelCost(rent){
-//     var night = 0;
-// if(rent<=10){
-//     night = rent * 100;
-// }
-// else if(rent<=20){
-//     var firstnight = 10 * 100;
-//     var remaining = rent - 10;
-//     var secondnight = remaining * 80;
-//     night = firstnight + secondnight;
-// }
-// else{
-//     var firstnight = 10 * 100;
-//     var secondnight = 10 * 80;
-//     var remaining = rent - 20;
-//     var thirdnight = remaining * 50;
-//     night = firstnight + secondnight + thirdnight;
-// }
-//   return night;
-// }
-// var total = hotelCost(25);
+function hotelCost(rent){
+    var night = 0;
+if(rent<=10){
+    night = rent * 100;
+}
+else if(rent<=20){
+    var firstnight = 10 * 100;
+    var remaining = rent - 10;
+    var secondnight = remaining * 80;
+    night = firstnight + secondnight;
+}
+else{
+    var firstnight = 10 * 100;
+    var secondnight = 10 * 80;
+    var remaining = rent - 20;
+    var thirdnight = remaining * 50;
+    night = firstnight + secondnight + thirdnight;
+}
+  return night;
+}
+var total = hotelCost(25);
 
-// console.log(total);
+console.log(total);
 
 
-// megaFriend
-
+     megaFriend
 
 function findLongestWord(str){
   var max = str[0];
